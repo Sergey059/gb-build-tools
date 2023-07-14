a4912d2a3ec9b526cd31534edf6a884dcc6d487a
Sergey059 <sergeiv-82@mail.ru>
2023-07-14 16:39:03 +0500

    finished

---
+++ src/cards.js
@@ -0,0 +1,23 @@
+import img1 from "./media/images/1.jpg";
+import img2 from "./media/images/2.jpg";
+import img3 from "./media/images/3.jpg";
+import img4 from "./media/images/4.gif";
+
+import audio1 from "./media/audio/1.mp3";
+import audio2 from "./media/audio/2.mp3";
+import audio3 from "./media/audio/3.mp3";
+
+import video1 from "./media/video/1.mp4";
+import video2 from "./media/video/2.mp4";
+
+export default [
+  { title: "Мельница у реки", type: "image", src: img1 },
+  { title: "Горы осенью", type: "image", src: img2 },
+  { title: "Деревья летом", type: "image", src: img3 },
+  { title: "Анимированный котик", type: "image", src: img4 },
+  { title: "Птички летом", type: "audio", src: audio1 },
+  { title: "Ручей", type: "audio", src: audio2 },
+  { title: "Закат в лесу", type: "audio", src: audio3 },
+  { title: "Потанцули", type: "video", src: video1 },
+  { title: "Прибой", type: "video", src: video2 },
+];
